286c77a1d77bd9fa025fba98708cc4a95aaffaec
Byungyong Park <qkrquddyd94@gmail.com>
2022-09-01 15:22:27 +0900

    STUDY [TopAppBars] FilterTasksMenu, TopAppBarDropdownMenu 생성

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/util/TopAppBars.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/util/TopAppBars.kt
@@ -1,14 +1,16 @@
 package com.example.android.architecture.blueprints.todoapp.util
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.example.android.architecture.blueprints.todoapp.R
 
@@ -27,8 +29,50 @@ fun TasksTopAppBar(
             }
         },
         actions = {
-            // TODO
+            FilterTasksMenu()
         },
         modifier = Modifier.fillMaxWidth()
     )
+}
+
+@Composable
+private fun FilterTasksMenu(
+
+) {
+    TopAppBarDropdownMenu(
+        iconContent = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_filter_list),
+                contentDescription = stringResource(id = R.string.menu_filter),
+            )
+        }
+    ) {
+        // TODO
+    }
+}
+
+/*
+* @Composable ColumnScope.(() -> Unit) -> Unit?
+* by remember { mutableStateOf(false) }?
+* Modifier.wrapContentSize(Alignment.TopEnd)?
+* */
+@Composable
+private fun TopAppBarDropdownMenu(
+    iconContent: @Composable () -> Unit,
+    content: @Composable ColumnScope.(() -> Unit) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.wrapContentSize(Alignment.TopEnd)) {
+        IconButton(onClick = { expanded = !expanded }) {
+            iconContent()
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.wrapContentSize(Alignment.TopEnd),
+        ) {
+            content { expanded = !expanded }
+        }
+    }
 }
