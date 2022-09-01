8bc4480dd11435f410d35f488a100d59176919be
Byungyong Park <qkrquddyd94@gmail.com>
2022-09-01 15:22:27 +0900

    STUDY [TodoNavGraph] NavHost의 NavGraphBuilder에 composable 추가

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoNavGraph.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoNavGraph.kt
@@ -2,20 +2,45 @@ package com.example.android.architecture.blueprints.todoapp
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.android.architecture.blueprints.todoapp.TodoDestinationsArgs.USER_MESSAGE_ARG
 
 /*
 * TodoNavGraph?
 * NavHostController?
 * rememberNavController?
+* NavHost?
+* composable?
+* NavBackStackEntry?
 * */
 @Composable
 fun TodoNavGraph(
+    modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
     startDestination: String = TodoDestinations.TASKS_ROUTE,
 ) {
     val currentNavBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = currentNavBackStackEntry?.destination?.route ?: startDestination
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier,
+    ) {
+        composable(
+            route = TodoDestinations.TASKS_ROUTE,
+            arguments = listOf(
+                navArgument(name = USER_MESSAGE_ARG) { type = NavType.IntType; defaultValue = 0 }
+            ),
+        ) { entry ->
+
+        }
+    }
 }
